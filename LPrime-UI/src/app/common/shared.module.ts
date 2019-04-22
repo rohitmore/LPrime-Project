efec876bf96022cb93896fb5ebca0cfdf2edff06
Rohit More <deepakmore@outlook.com>
2019-04-22 17:43:07 +0530

    Added Products Page and back to Top button

--- LPrime-UI/src/app/common/shared.module.ts
+++ LPrime-UI/src/app/common/shared.module.ts
@@ -1,14 +1,20 @@
 import {NgModule} from '@angular/core';
 import {StringTrimmerDirective} from "./directive/string-trimmer/string-trimmer.directive";
+import { BackToTopComponent } from './components/back-to-top/back-to-top.component';
+import {FormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+import {MaterialModule} from "../material-module/material.module";
 
 @NgModule({
   declarations: [
-    StringTrimmerDirective
+    StringTrimmerDirective,
+    BackToTopComponent
   ],
-  imports: [ ],
+  imports: [ BrowserModule, FormsModule, MaterialModule ],
   providers: [ ],
   exports: [
-    StringTrimmerDirective
+    StringTrimmerDirective,
+    BackToTopComponent
   ]
 })
 export class SharedModule { }

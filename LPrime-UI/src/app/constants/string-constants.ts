f9507d57bebec8acac18f48796b1f57fffb1aa1c
Rohit More <deepakmore@outlook.com>
2019-04-28 01:35:57 +0530

    Added constant messages and maintained responsive nature for product info page

--- LPrime-UI/src/app/constants/string-constants.ts
+++ LPrime-UI/src/app/constants/string-constants.ts
@@ -1,3 +1,4 @@
 export const COMPANY_NAME : string = 'L Prime';
 
 export const ADD_TO_WISHLIST : string = 'Product added successfully in wishlist';
+export const ADD_TO_CART : string = 'Product added successfully in cart';
--- LPrime-UI/src/app/product-grid/product-info/product-info.component.ts
+++ LPrime-UI/src/app/product-grid/product-info/product-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatSnackBar} from "@angular/material";
+import * as consts from "../../constants/string-constants";
 
 @Component({
   selector: 'app-product-info',
@@ -7,17 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductInfoComponent implements OnInit {
 
-  images : [
+  images = [
       'https://cdn.shopify.com/s/files/1/1047/6452/products/product_1024x1024.png?v=1446769025',
       'https://cdn.shopify.com/s/files/1/1047/6452/products/tricko1_1024x1024.jpg?v=1447104179',
       'https://cdn.shopify.com/s/files/1/1047/6452/products/tricko2_1024x1024.jpg?v=1447104180',
       'https://cdn.shopify.com/s/files/1/1047/6452/products/tricko3_1024x1024.jpg?v=1447104182'
     ];
-  constructor() {
+
+  durationInSeconds = 15;
+  constants = consts;
+  constructor(private snackBar: MatSnackBar) {
 
   }
 
   ngOnInit() {
+  }
+
+  addToWishList(message: string, action?: string) {
+    this.snackBar.open(message, action, {
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['snack-bar']
+    });
+  }
+
+  addToCart(message: string, action?: string) {
+    this.snackBar.open(message, action, {
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['snack-bar']
+    });
   }
 
 }
